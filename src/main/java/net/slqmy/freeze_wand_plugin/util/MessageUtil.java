e00fbb0a69bf4072fa2414d7740240665c307eae
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-04 17:45:38 +0100

    Delete unused file

--- src/main/java/net/slqmy/freeze_wand_plugin/util/MessageUtil.java
+++
@@ -1,9 +0,0 @@
-package net.slqmy.freeze_wand_plugin.util;
-
-import org.jetbrains.annotations.NotNull;
-
-public final class MessageUtil {
-	public static @NotNull String translateChatColours(@NotNull final String message) {
-		return message.replace('&', '\u00A7');
-	}
-}
